ec41070ae3c5cec1a7520a40f545a5c74e30b073
HBSTUDIO <hbstudio@naver.com>
2022-07-23 14:12:37 +0900

    Tempate Test

--- template_test.cpp
+++ template_test.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-void fPrint1(int);	//일반함수
+void fPrint1(int);	//일반함수 #1
 
 template <class T>
-void fPrint2(T);	//Template Function
+void fPrint2(T);	//Template Function #2, #3
+
+void fPrint2(double);	//function override #3
 
 
 int main(void)
@@ -14,6 +16,15 @@ int main(void)
 	fPrint1(n);
 	puts("2. Template Function - generic");
 	fPrint2(n);
+	//if... double type input to func...
+	/*double k = 10.0;
+	fPrint2(k);*/	//compile warning
+	
+	//case3.. override
+	puts("3. Function Override");
+	double k = 10.0;
+	fPrint2(k);			//no warning.. 
+
 	return 0;
 }
 
@@ -26,4 +37,9 @@ template <class T>
 void fPrint2(T n)
 {
 	printf("%d\n",n);
+}
+
+void fPrint2(double n)
+{
+	printf("%.1f\n", n);
 }
