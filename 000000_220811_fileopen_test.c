915feeb63933c31b36cfaf6001187e5b836f3bd3
HBSTUDIO <hbstudio@naver.com>
2022-08-13 20:50:21 +0900

    코딩수정

--- 000000_220811_fileopen_test.c
+++ 000000_220811_fileopen_test.c
@@ -6,12 +6,26 @@ int main(void) {
 
 	//char* fName = "double.txt";
 	FILE* fp;
-	fp = fopen("double.txt", "r");
+	fp = fopen("double.txt", "rb");
 	int ch;
-	while ((ch = getc(fp)) != EOF)
-	{
-		putchar(ch);
+	double tmp;
+
+	if (fp == NULL) {
+		puts("파일을 열수 없습니다.");
+		return 0;
 	}
+	else
+		puts("파일 열기 성공");
+
+	//while ((ch = getc(fp)) != EOF)
+	//{
+	//	putchar(ch);
+	//}
+
+	int outV = fread(&tmp, sizeof(double), 1, fp);
+
+	printf("%d", outV);
+
 	fclose(fp);
 
 	return 0;
