041f2af52c6ce21e44e006553163a30b2e31ffae
HBSTUDIO <hbstudio@naver.com>
2023-01-19 23:04:59 +0900

    하노이타워

--- 000000_230118_IDSU_HANOI_TOWER.c
+++ 000000_230118_IDSU_HANOI_TOWER.c
@@ -2,31 +2,26 @@
 #pragma warning(disable:6031)
 #include <stdio.h>
 
-#define DEFAULTSIZE 3				//하노이타워 원반의 기본 갯수
-static int _iCrops = DEFAULTSIZE;	//언제든지 바꿀 수 있게 전역변수에 static으로 선언 해준다.
 
-//하노이타워
-//타워의 높이를 입력 받는다
-//진행되는 step을 하나씩 출력 한다.
-//원반의 크기를 숫자로 표시한다.
-//기본 디폴트 크기는 3 >> 선택을 안했을 시
-//setting
-
-void _fSetting(void);
+void HanoiTowerMove(int, char, char, char);
 
 int main() {
 
-	_fSetting();
-	printf("%d", _iCrops);
+	HanoiTowerMove(4, 'A', 'B', 'C');
 
 	return 0;
 }
 
-void _fSetting(void) {
 
-	printf("==================[SETTING]================\n");
-	printf("1. Input Tower height (default:%d):__\b\b",_iCrops);
-	scanf("%d", &_iCrops);
-	getchar();
+void HanoiTowerMove(int num, char from, char by, char to) {
+
+	if (num == 1) {
+		printf("원반 1을 %c에서 %c로 이동 \n", from, to);
+	}
+	else {
+		HanoiTowerMove(num - 1, from, to, by);
+		printf("원반 %d을(를) %c에서 %c로 이동 \n", num, from, to);
+		HanoiTowerMove(num - 1, by, from, to);
+	}
 
 }
