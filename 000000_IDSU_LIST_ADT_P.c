f1d2e7c0299d8de3b234d48ec922a01973e615ab
HBSTUDIO <hbstudio@naver.com>
2023-01-30 22:48:46 +0900

    [ING]_LFirst Function

--- 000000_IDSU_LIST_ADT_P.c
+++ 000000_IDSU_LIST_ADT_P.c
@@ -9,7 +9,7 @@ void ListInit(LIST* plist) {
 	plist->numOfData = 0;
 	puts("초기화 완료");
 }
-void Linsert(LIST* plist, LDATA data) {
+void Linsert(LIST* plist, LDATA * data) {
 	if (plist->numOfData < LIST_LEN) {			//LIST_LEN이하일때만 저장한다.
 		plist->arr[plist->numOfData] = data;	//데이터를 추가
 		(*plist).numOfData++;					//저장 수 증가
@@ -23,6 +23,7 @@ int LFirst(LIST* plist, LDATA* pdata) {	//data 조회
 	if (plist->numOfData == 0) {	//만약 저장이 안된 자료 구조라면?
 		return FALSE;				//False를 리턴한다.
 	}
+	//아니라면 첫번째 data를 조회하여 값을 리턴한다.
 
 }
 int LNext(LIST* plist, LDATA* pdata) {
