7c197c65f54a03f3a46deb547a702d41ba0695c4
HBSTUDIO <hbstudio@naver.com>
2023-07-20 23:13:09 +0900

    가로세로

--- 000000_230718_Practice1.c
+++ 000000_230718_Practice1.c
@@ -2,14 +2,17 @@
 
 // 정보처리기사 실기 대비
 
-
-
-
 int main(void) {
 
 	//Q1. 가로 세로~
 	int arr1[5][5] = { 0, };
 
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("%d,", arr1[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
